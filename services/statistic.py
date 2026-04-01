8caf9a36290975d0cc65408a1ccab14939922150
Dmytro Pernai <pernai.dmytriy@gmail.com>
2026-04-01 09:02:06 +0200

    Changed the performance of the last 5 requests statistics

--- services/statistic.py
+++ services/statistic.py
@@ -8,10 +8,8 @@ def get_top_searches(limit=5):
     query = [
         {
             "$group": {
-                "_id": {
-                    "search_type": "$search_type",
-                    "param": "$param"
-                },
+                "_id": {"search_type": "$search_type",
+                        "param": "$param"},
                 "count": {"$sum": 1}
             }
         },
@@ -30,6 +28,20 @@ def get_last_searches(limit=5):
     collection = mongo_client.get_collection(config.MONGO_COLLECTION)
 
     query = [
+
+        {
+            "$sort": {"timestamp": -1}
+        },
+        {
+            "$group": {
+                "_id": {"search_type": "$search_type",
+                        "param": "$param"},
+                "new_file": {"$first": "$$ROOT"}
+            }
+        },
+        {
+            "$replaceRoot": {"newRoot": "$new_file"}
+        },
         {
             "$sort": {"timestamp": -1}
         },
